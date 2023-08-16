b6ac503f516e187ae3b29bf0a6b5db666634d915
Ahmadok99 <ahmadoxk@gmail.com>
2023-08-16 11:55:14 +0300

    edited

--- jscode.js
+++ jscode.js
@@ -1,19 +1,106 @@
 
-const btnNav = document.getElementById("btnN")
+const buttonNav = document.getElementById("button-nav")
 const navbar = document.querySelector('.mainnavbar');
-const buttoncart = document.getElementById("myButton")
-const nav = document.getElementById('cartdiv')
-const cartButton = document.getElementById('myButton');
-const cartDiv = document.getElementById('cartdiv');
+const buttoncart = document.getElementById("my-button")
+const nav = document.getElementById('cart-div')
+const cartButton = document.getElementById('my-button');
+const cartdiv = document.getElementById('cart-div');
 const itemsContainer = document.querySelector('.items3');
 
 
 
 
+//Function >>>>
+
+
+/**
+* Generate a unique ID using timestamp and random number.
+* @returns {string} The unique ID.
+*/
+const generateUniqueId = () => {
+    const timestamp = new Date().getTime();
+    const random = Math.floor(Math.random() * 100000); 
+    return `${timestamp}-${random}`;
+}
+
+
+/**
+* Add an item to the cart.
+* 
+ * @param {Object} item - The item to be added to the cart.
+ * 
+ * @return {void} 
+*/
+const addToCart = (item) => {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+    const uniqueId = generateUniqueId();
+    item.id = uniqueId;
+    cartItems.push(item);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
+
+
+/**
+ * Display the items in the cart.
+ * 
+ */
+const displayCart = () => {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+    cartdiv.innerHTML = '';
+    let total = 0; 
+    let itemCount = 0; 
+    if (!cartItems.length) {
+        cartdiv.innerHTML = '<p>Your cart is empty.</p> Total: 0  Item Count : 0';
+        total.textContent = '0'
+        itemCount.textContent = '0'
+        return;
+    }
+
+    cartItems.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'listcart';
+
+        const itemImage = document.createElement('img');
+        itemImage.src = item.imageSrc;
+        itemImage.style.width = '3rem';
+        itemImage.style.height = '3rem';
+        itemDiv.appendChild(itemImage);
+
+        const itemName = document.createElement('span');
+        itemName.textContent = item.name;
+        itemDiv.appendChild(itemName);
+
+        const itemPrice = document.createElement('h4');
+        itemPrice.textContent = item.price;
+        itemDiv.appendChild(itemPrice);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('deleteBt');
+        deleteButton.setAttribute('data-id', item.id);
+        deleteButton.addEventListener('click', () => removeFromCart(item.id));
+        itemDiv.appendChild(deleteButton);
+
+        total += parseFloat(item.price.replace('$', ''));
+        itemCount++;
+        cartdiv.appendChild(itemDiv);
+    });
+
+    const cartSummary = document.createElement('div');
+    cartSummary.className = 'cart-summary';
+    cartSummary.innerHTML = `<p>Total: $${total.toFixed(2)} | Item Count: ${itemCount}</p>`;
+    cartdiv.appendChild(cartSummary);
+}
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
+/**Create Cards in store.
+ * 
+ * @param {object} 
+ * 
+ * @return {void}
+ */
+const createCards = () => {
     const itemsContainer = document.querySelector('.items3');
     const cardsData = [
         { category: 'cake', image: 'Images/12.jpeg', name: 'Cake Item', price: '$85', id: '12' },
@@ -30,10 +117,11 @@ document.addEventListener('DOMContentLoaded', function () {
         { category: 'cupcake', image: 'Images/20.jpeg', name: 'CupCake Item', price: '$900', id: '1' },
     ];
 
+
     cardsData.forEach(data => {
+
         const uniqueId = generateUniqueId();
         data.id = uniqueId;
-
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card2');
         cardDiv.setAttribute('data-category', data.category);
@@ -49,7 +137,7 @@ document.addEventListener('DOMContentLoaded', function () {
         cardImage.appendChild(image);
 
         const bt = document.createElement('button');
-        bt.className = 'bti'
+        bt.className = 'button-cart'
         bt.innerHTML = '&#128722'
 
         const cardBody = document.createElement('div');
@@ -68,46 +156,104 @@ document.addEventListener('DOMContentLoaded', function () {
         cardDiv.appendChild(cardBody);
         itemsContainer.appendChild(cardDiv);
     });
+}
 
-    buttoncart.addEventListener('click', function (event) {
-        event.preventDefault();
-        if (nav.style.display === "block") {
-            nav.style.display = "none";
+
+createCards()
+const carditem = document.querySelectorAll('.card2');
+const categoryBtn = document.querySelectorAll('.cat');
+
+/**
+ *Filter and display the cards based on the selected category.
+*
+*  @param {string} category - The category to filter by.
+* 
+*  @return {void}  filtered category.
+*/
+const filterCard = (category) => {
+    carditem.forEach(card => {
+        
+        const cardcategory = card.getAttribute('data-category');
+        if (category === 'all' || cardcategory === category) {
+            card.style.display = 'block';
         } else {
-            nav.style.display = "block";
+            card.style.display = 'none';
         }
-    })
-
-    const carditem = document.querySelectorAll('.card2');
-    const categoryBtn = document.querySelectorAll('.cat');
+    });
+}
 
 
+/**
+* Remove an item from the cart based on its ID and update the cart display.
+* 
+* @param {string} id - The ID of the item to be removed.
+* 
+* @return {void} list cart items after remove an item
+*/
+const removeFromCart = (id) => {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+    const updatedCartItems = cartItems.filter(cartItem => cartItem.id !== id); 
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+    displayCart();
+}
 
 
-    /**
-     * Filter and display the cards based on the selected category.
-     * @param {string} category - The category to filter by.
-    */
-    function filterCard(category) {
-        carditem.forEach(card => {
-            const cardcategory = card.getAttribute('data-category');
-            if (category === 'all' || cardcategory === category) {
-                card.style.display = 'block';
-            } else {
-                card.style.display = 'none';
-            }
-        });
-    }
+
+
+document.addEventListener('DOMContentLoaded', function () {
     filterCard('all')
 
-    categoryBtn.forEach(button => {
+    //memory initialization .
+    if (!localStorage.getItem('cartItems')) {
+        localStorage.setItem('cartItems', JSON.stringify([]));
+    }
+});
+
+
+
+//Hidde and Show the nav bar .
+buttonNav.onclick = function () {
+        navbar.classList.toggle("showNav");
+}
+
+
+//Hidde and Show the ncart list .
+cartButton.addEventListener('click', (event) => {
+        displayCart();
+        event.preventDefault();
+        if (nav.style.display === "none") 
+        {nav.style.display = "block";} 
+        else
+        {nav.style.display = "none";}
+});
+
+
+
+
+// List item card filtered by category .
+categoryBtn.forEach(button => {
         button.addEventListener('click', function () {
             const cateogryselected = this.textContent.toLowerCase();
             filterCard(cateogryselected)
         });
-    });
+});
 
-    searchInput.addEventListener('input', function () {
+
+//Add item to cart .
+itemsContainer.addEventListener('click', event => {
+    if (event.target.classList.contains('button-cart')) {
+        const itemDiv = event.target.closest('.card2');
+        const itemName = itemDiv.querySelector('h5').textContent;
+        const itemPrice = itemDiv.querySelector('h4').textContent;
+        const itemImageSrc = itemDiv.querySelector('img').src;
+        addToCart({ name: itemName, imageSrc: itemImageSrc, price: itemPrice });
+    }
+    displayCart()
+});
+
+
+//Search in card . 
+searchInput.addEventListener('input', function () {
         const info = searchInput.value.toLowerCase();
         const filter = Array.from(carditem).filter(card => {
             const cardName = card.querySelector('.cbody h5').textContent.toLowerCase();
@@ -121,136 +267,7 @@ document.addEventListener('DOMContentLoaded', function () {
         filter.forEach(card => {
             card.style.display = 'block';
         });
-    });
-    if (!localStorage.getItem('cartItems')) {
-        localStorage.setItem('cartItems', JSON.stringify([]));
-    }
-
-
-
-    /**
-    * Add an item to the cart.
-     * @param {Object} item - The item to be added to the cart.
-    */
-    function addToCart(item) {
-        const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-        const uniqueId = generateUniqueId(); // Generate a unique ID for each item
-        item.id = uniqueId;
-        cartItems.push(item);
-        localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    }
-
-
-
-
-    /**
-     * Display the items in the cart.
-     */
-    function displayCart() {
-        const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-        cartDiv.innerHTML = '';
-        let total = 0; // Initialize the total price
-        let itemCount = 0; // Initialize the item count
-        if (!cartItems.length) {
-            cartDiv.innerHTML = '<p>Your cart is empty.</p> Total: 0  Item Count : 0'  ;
-            total.textContent='0'
-            itemCount.textContent='0'
-            return;
-        }
-
-        cartItems.forEach(item => {
-            const itemDiv = document.createElement('div');
-            itemDiv.className = 'listcart';
-
-            const itemImage = document.createElement('img');
-            itemImage.src = item.imageSrc;
-            itemImage.style.width = '3rem';
-            itemImage.style.height = '3rem';
-            itemDiv.appendChild(itemImage);
-
-            const itemName = document.createElement('span');
-            itemName.textContent = item.name;
-            itemDiv.appendChild(itemName);
-
-            const itemPrice = document.createElement('h4');
-            itemPrice.textContent = item.price;
-            itemDiv.appendChild(itemPrice);
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.classList.add('deleteBt');
-            deleteButton.setAttribute('data-id', item.id); 
-            deleteButton.addEventListener('click', () => removeFromCart(item.id)); 
-            itemDiv.appendChild(deleteButton);
-
-            total += parseFloat(item.price.replace('$', '')); 
-            itemCount++;
-            cartDiv.appendChild(itemDiv);
-
-            
-
-        }); 
-        
-        const cartSummary = document.createElement('div');
-        cartSummary.className = 'cart-summary';
-        cartSummary.innerHTML = `<p>Total: $${total.toFixed(2)} | Item Count: ${itemCount}</p>`;
-        
-        cartDiv.appendChild(cartSummary);
-    }
-
-
-    itemsContainer.addEventListener('click', event => {
-        if (event.target.classList.contains('bti')) {
-            const itemDiv = event.target.closest('.card2');
-            const itemName = itemDiv.querySelector('h5').textContent;
-            const itemPrice = itemDiv.querySelector('h4').textContent;
-            const itemImageSrc = itemDiv.querySelector('img').src;
-            addToCart({ name: itemName, imageSrc: itemImageSrc, price: itemPrice });
-        }
-        displayCart()
-    });
-
-
-    cartButton.addEventListener('click', () => {
-        displayCart();
-        cartDiv.style.display = 'block';
-    });
-
-
-    /**
-     * Remove an item from the cart based on its ID and update the cart display.
-     * @param {string} id - The ID of the item to be removed.
-     */
-    function removeFromCart(id) {
-        const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-        const updatedCartItems = cartItems.filter(cartItem => cartItem.id !== id); // Compare with the provided ID
-        localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
-        displayCart();
-    }
-
-    btnNav.onclick = function () {
-        navbar.classList.toggle("showNav");
-    }
-    /**
- * Generate a unique ID using timestamp and random number.
- * @returns {string} The unique ID.
- */
-    function generateUniqueId() {
-        const timestamp = new Date().getTime(); // Get current timestamp
-        const random = Math.floor(Math.random() * 100000); // Generate a random number
-        return `${timestamp}-${random}`;
-    }
-
-
 });
-
-
-
-
-
-
-
-
 
 
 
